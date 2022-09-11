f538252c020afd98d252d3e4f0df3228823479a3
Adan Serrano <adanu0503@gmail.com>
2022-09-11 00:39:01 -0500

    primer commit

---
+++ SECCIONES/SECCION1/app.js
@@ -0,0 +1,10 @@
+// console.log('Hola Mundo');
+
+// estamos creando una variable
+let a = 10,
+    b = 20,
+    x = a + b;
+console.log( x );
+console.warn( x );
+console.error( x );
+const c = 10; //una constante no se puede modificar 
