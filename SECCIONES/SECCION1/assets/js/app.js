80aa74f2e47a2840e94a0deb691033b46b93b6b3
Adan Serrano <adanu0503@gmail.com>
2022-10-04 20:23:51 -0500

    Seccion 2 agregada

---
+++ SECCIONES/SECCION1/assets/js/app.js
@@ -0,0 +1,30 @@
+alert('hola mundo desde app.js');
+
+// console.log('Hola Mundo');
+
+// estamos creando una variable
+//PARA HACER VARIABLES DE TODO TIPO
+let a = 10,
+b = 20,
+c = 'spiderman ',
+d = 'Adán'
+x = a + b;
+
+
+const saludo = c + d;
+
+// // imprimir un Valor
+//     console.log( x );
+// // imprimir un valor con simbolo de warning 
+//     console.warn( x );
+// // imprimir un valor con simbolo de error 
+//     console.error( x );
+// const c = 10; //una constante no se puede modificar 
+// console.log( '%c Mis variables', 'color: white; font-weigth=bold' );
+// console.log('x =', x);
+// console.table({ a, b,c, d, x });
+// console.log( saludo );
+
+c = 'hola de nuevo';
+
+let miNuevoNombre = 'Adán';
