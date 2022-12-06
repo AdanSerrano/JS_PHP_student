6c9f33cdda816c2573707f19bc2e905a5889ed45
Adan Serrano <adan.serrano@utp.ac.pa>
2022-12-05 19:35:23 -0500

    inicio con php

---
+++ PHP/01-hola-mundo.php
@@ -0,0 +1,16 @@
+<?php include 'includes/header.php';
+
+    // echo "Hola mundo";
+
+
+    // print "Hola mundo";
+
+    // print("Hola mundo");
+
+    // print_r("Hola mundo");
+
+
+    // var_dump("Hola mundo"); // Muestra el tipo de dato y el valor
+    
+include 'includes/footer.php';
+?>
