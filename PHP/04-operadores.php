64c602a9e3d4373a37d2d1faff6046e7a4cb3d53
Adan Serrano <adan.serrano@utp.ac.pa>
2022-12-06 13:59:09 -0500

    operadores en php

--- PHP/04-operadores.php
+++ PHP/04-operadores.php
@@ -1,6 +1,43 @@
 <?php include 'includes/header.php';
 
 
+// operadores 
+$num1 = 20;
+$num2 = 30;
+
+// sumar 
+echo $num1 + $num2;
+
+/// restar
+echo $num1 - $num2;
+
+// multiplicar
+echo $num1 * $num2;
+
+// dividir
+echo $num1 / $num2;
+
+// modulo
+echo $num1 % $num2;
+
+// incrementar
+echo $num1++;
+
+// decrementar
+echo $num1--;
+
+// concatenar
+echo $num1 . $num2;
+
+// comparar
+echo $num1 == $num2;
+
+// comparar identico
+echo $num1 === $num2;
+
+// comparar diferente
+echo $num1 != $num2;
+
 
 
 include 'includes/footer.php';
