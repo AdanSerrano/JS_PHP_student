aee41aaf3edfa919ceb880c3dff17dc1fb7ba20e
Adan Serrano <adan.serrano@utp.ac.pa>
2022-12-06 14:59:49 -0500

    arreglos asociativos

--- PHP/09-arrays-assoc.php
+++ PHP/09-arrays-assoc.php
@@ -1,6 +1,30 @@
 <?php include 'includes/header.php';
+// arreglos asociativos 
+$clientes = [
+    'nombre' => 'Adan',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'Premium',
+        'disponible' => 100
+    ]
+];
+
+echo "<pre>";
+var_dump($clientes['nombre']);
+echo "</pre>";
 
 
+// acceder a valores de un array asociativo 
+echo $clientes['nombre'];
+echo $clientes['saldo'];
 
+echo $clientes['informacion']['tipo'];
+
+
+//
+$clientes['codigo'] = 12345;
+echo "<pre>";
+var_dump($clientes);
+echo "</pre>";
 
 include 'includes/footer.php';
