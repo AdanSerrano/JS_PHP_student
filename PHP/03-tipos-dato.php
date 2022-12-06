a9f4e9ea4b2724a3a504c212af26b59e049c4270
Adan Serrano <adan.serrano@utp.ac.pa>
2022-12-06 13:51:57 -0500

    tipo de datos

--- PHP/03-tipos-dato.php
+++ PHP/03-tipos-dato.php
@@ -1,6 +1,28 @@
 <?php include 'includes/header.php';
 
+// bolean 
+$logueado = false;
+ 
+var_dump($logueado);
 
 
+// Enteros
+$numero = 200;
+var_dump($numero); 
+
+
+// Flotantes
+$float = 200.5;
+var_dump($float);
+
+
+// Strings
+$nombre = "Adan";
+var_dump($nombre);
+
+
+// Arreglos
+$array = [];
+var_dump($array);
 
 include 'includes/footer.php';
