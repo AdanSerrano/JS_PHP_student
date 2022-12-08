120b3ec8fba6f538d533f4ea638a9b50a4a5ef18
Adan Serrano <adan.serrano@utp.ac.pa>
2022-12-07 21:08:15 -0500

    Types PHP

--- PHP/16-types.php
+++ PHP/16-types.php
@@ -1,7 +1,30 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types=1); // Para que no se pueda pasar un string a una funcion que espera un int
+include 'includes/header.php';
 
 
+function usuarioAutenticado(bool $autenticado) : ?string{ // define el tipo de dato que va a retornar la funcion
+    if($autenticado){
+        return "El usuario esta autenticado";
+    } else {
+        return null;
+    }
+}
 
+$usuario = usuarioAutenticado(true);
+echo $usuario;
+echo "<br>";
 
+function usuarioAutenticado1(bool $autenticado): string|int
+{ // define el tipo de dato que va a retornar la funcion
+    if ($autenticado) {
+        return "El usuario esta autenticado";
+    } else {
+        return 20;
+    }
+}
+
+$usuario2 = usuarioAutenticado1(false);
+echo $usuario2;
 
 include 'includes/footer.php';
